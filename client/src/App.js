b5fd05379ee7c7afa530052e48c73c75afcfa91c
Jarod Johnson <93343444+jarod-johnson-23@users.noreply.github.com>
2022-12-13 13:46:35 -0700

    Added components
    -Added footer and header components

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about-us" element={<About />} />
           <Route path="/contact-us" element={<Contact />} />
           <Route path="*" element={<Home />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
